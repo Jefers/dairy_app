fc50325b27c4806593b654928013f070f6a5c943
Jeff Gordon <m.j.l.gordon@googlemail.com>
2011-02-08 13:58:52 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DairyApp::Application.config.secret_token = 'd70fe8e62bcff8350fc56ed6346869364478372f9c339a63427cb7720e6cd2d021626eafb160fd6f6b13dcf62fb2284679238ce473fe953f66afd98b1e740d2f'
