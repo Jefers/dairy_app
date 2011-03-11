8693431edd0fdaba909429825e9e8c8ed418c14d
Jeff Gordon <m.j.l.gordon@googlemail.com>
2011-03-11 11:17:45 +0000

    layout pages changed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,27 +6,43 @@ class PagesController < ApplicationController
 
   def contact
     @title = "Contact"
+    render :layout => 'two_column_page_page_layout'
   end
 
   def about
     @title = "About"
+    render :layout => 'two_column_page_layout'
   end
 
   def help
     @title = "Help"
+    render :layout => 'two_column_page_layout'
   end
 
   def disclaimer
     @title = "Disclaimer"
+    render :layout => 'two_column_page_layout'
   end
 
   def privacy
     @title = "Privacy"
+    render :layout => 'two_column_page_layout'
   end
 
   def security
     @title = "Security"
+    render :layout => 'two_column_page_layout'
   end
+
+  def fun
+    @title = "Fun"
+    render :layout => 'full_page_layout'
+  end
+
+  def news
+    @title = "News"
+  end
+
 
 end
 
