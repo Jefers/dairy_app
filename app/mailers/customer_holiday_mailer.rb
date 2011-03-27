0d5a1b7fbdad032b8f80dbb64279a5fc7903533c
Jeff Gordon <m.j.l.gordon@googlemail.com>
2011-03-27 13:00:53 +0100

    cosmetic email

--- app/mailers/customer_holiday_mailer.rb
+++ app/mailers/customer_holiday_mailer.rb
@@ -2,9 +2,12 @@ class CustomerHolidayMailer < ActionMailer::Base
   default :from => "milkman@waldendairy.com"
 
   def customer_holiday_email(customer_holiday)
+    data = File.read(Rails.root.join('public/images/small_wd.png'))
+    attachments.inline['small_wd.png'] = data
+
     @customer_holiday  = customer_holiday
     @url  = "http://waldendairy.com/sign_in"
-    mail(:to => customer_holiday.email_address,
+    mail(:to => "#{customer_holiday.name} <#{customer_holiday.email_address}>",
          :cc => "milkman@waldendairy.com",
          :reply_to => "problem@waldendairy.com",
          :subject => "Holiday dates")
