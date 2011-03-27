a20a1b05c3cff0074a508921797b5961602e4119
Jeff Gordon <m.j.l.gordon@googlemail.com>
2011-03-27 14:20:21 +0100

    cosmetic order email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,6 +2,8 @@ class OrderMailer < ActionMailer::Base
   default :from => "milkman@waldendairy.com"
 
   def order_email(order)
+    data = File.read(Rails.root.join('public/images/small_wd.png'))
+    attachments.inline['small_wd.png'] = data
     @order = order
     @url  = "http://waldendairy.com/sign_in"
     mail(:to => order.email,
