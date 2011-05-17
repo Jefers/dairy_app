5d4e1e31ab190949a9632c4f38fe3114cd15f619
Jeff Gordon <m.j.l.gordon@googlemail.com>
2011-05-17 21:21:25 +0100

    cosmetic 17-05-2011 2

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,13 +1,5 @@
 # # config/initializers/setup_mail.rb
-# ActionMailer::Base.smtp_settings = {
-#   :address              => "smtp.gmail.com",
-#   :port                 => 587,
-#   :domain               => "waldendairy.com",
-#   :user_name            => "milkman",
-#   :password             => "secret",
-#   :authentication       => "plain",
-#   :enable_starttls_auto => true
-# }
+
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
